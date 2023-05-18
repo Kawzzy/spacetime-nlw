1cdcc30c7ee09c9c3d8292dbcb69275108214529
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2023-05-18 15:14:39 -0300

    Setup mobile.

---
+++ mobile/App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-zinc-950">
+      <Text className="text-5xl font-bold text-zinc-50">
+        Hello World, it's my first app!
+      </Text>
+      <StatusBar style="light" />
+    </View>
+  );
+}
