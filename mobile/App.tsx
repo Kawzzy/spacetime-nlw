f8258868b681276ad798de7929e455df0dae3d34
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2023-05-19 13:25:01 -0300

    Layout mobile initial config.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,13 +1,39 @@
+import { BaiJamjuree_700Bold } from "@expo-google-fonts/bai-jamjuree";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { ImageBackground, Text } from "react-native";
+
+import blurBg from "./src/assets/bg-blur.png";
+// Component created from a svg file
+import Stripes from "./src/assets/stripes.svg";
+import { styled } from "nativewind";
+
+const StyledStripes = styled(Stripes);
 
 export default function App() {
+  const [hasLoadedFonts] = useFonts({
+    Roboto_700Bold,
+    Roboto_400Regular,
+    BaiJamjuree_700Bold,
+  });
+
+  if (!hasLoadedFonts) {
+    return null;
+  }
+
   return (
-    <View className="flex-1 items-center justify-center bg-zinc-950">
-      <Text className="text-5xl font-bold text-zinc-50">
-        Hello World, it's my first app!
-      </Text>
-      <StatusBar style="light" />
-    </View>
+    <ImageBackground
+      source={blurBg}
+      className="relative flex-1 items-center bg-gray-900"
+      imageStyle={{ position: "absolute", left: "-100%" }}
+    >
+      <StyledStripes className="absolute left-2" />
+
+      <StatusBar style="light" translucent />
+    </ImageBackground>
   );
 }
