eb816372718905efffbb5f29d77deb08e60cc694
Augusto Kalahary <AuuKalaharyKW@gmail.com>
2023-05-21 19:20:19 -0300

    Authentication middleware config.

--- web/src/app/api/auth/callback/route.ts
+++ web/src/app/api/auth/callback/route.ts
@@ -6,6 +6,8 @@ export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url);
   const code = searchParams.get("code");
 
+  const redirectTo = request.cookies.get("redirectTo")?.value;
+
   const registerResponse = await api.post("/register", {
     code,
   });
@@ -14,8 +16,10 @@ export async function GET(request: NextRequest) {
 
   const cookieExpiresInSeconds = 60 * 60 * 24 * 30;
 
+  const redirectURL = redirectTo ?? new URL("/", request.url);
+
   // redirect to the home page with the token set as a cookie to be visible all around the application
-  return NextResponse.redirect(new URL("/", request.url), {
+  return NextResponse.redirect(redirectURL, {
     headers: {
       "Set-Cookie": `token=${token}; Path=/; max-age=${cookieExpiresInSeconds}`,
     },
